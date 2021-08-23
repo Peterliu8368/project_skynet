a77dd2d593f266c7d9c506d609f9e083bf8d5940
Jiaxin(Peter) Liu <peterliu8368@gmail.com>
2021-08-22 17:39:42 -0700

    adds video communication

---
+++ server.py
@@ -0,0 +1,24 @@
+from skynet_app import app
+from flask import render_template, redirect
+from flask_socketio import SocketIO, join_room, leave_room, send, emit
+from uuid import uuid4
+socketio = SocketIO(app)
+
+
+@app.route('/')
+def index():
+    return redirect(f'/{uuid4()}')
+
+@app.route('/<roomId>')
+def room(roomId):
+    return render_template('room.html', roomId = roomId)
+
+
+@socketio.on('join-room')
+def joinNewRoom(roomId, userId):
+    join_room(roomId)
+    emit('user-connected', userId, broadcast=True, to=roomId)
+
+if __name__ == '__main__':
+
+    socketio.run(app, debug=True, port=5000)
