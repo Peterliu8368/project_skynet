ba2a1b6004e9ee3a6f6822398b53e5303a841c1c
Jiaxin(Peter) Liu <peterliu8368@gmail.com>
2021-08-23 13:59:39 -0700

    adding text based chat function

--- server.py
+++ server.py
@@ -1,13 +1,19 @@
+from werkzeug.wrappers import request
 from skynet_app import app
-from flask import render_template, redirect
-from flask_socketio import SocketIO, join_room, leave_room, send, emit
+from flask import render_template, redirect, request
+from flask_socketio import SocketIO, join_room, leave_room, send, emit, disconnect
 from uuid import uuid4
 socketio = SocketIO(app)
 
 
 @app.route('/')
 def index():
-    return redirect(f'/{uuid4()}')
+    return render_template('index.html')
+
+@app.route('/process-join', methods=['post'])
+def processJoin():
+    roomName = request.form['room']
+    return redirect(f'/{roomName}')
 
 @app.route('/<roomId>')
 def room(roomId):
@@ -17,10 +23,19 @@ def room(roomId):
 def joinNewRoom(roomId, userId):
     join_room(roomId)
     emit('user-connected', userId, broadcast=True, to=roomId)
+    
 
-# @socketio.on('leave')
-# def disconnectUser(userId, roomId):
-#     emit('user-offline', userId, roomId, broadcast=True, to=roomId)
+@socketio.on('client-disconnect-request')
+def disconnection():
+    print('disconnection requet received, disconnecting the client')
+    emit('removal', broadcast=True)
+    emit('redirect-home')
+    
+
+@socketio.on('message')
+def sendMessage(message):
+    emit('create-message', message, broadcast=True)
+    print('message received')
 
 if __name__ == '__main__':
     socketio.run(app, debug=True, port=5000)
