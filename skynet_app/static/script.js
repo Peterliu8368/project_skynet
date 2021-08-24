006b1a075a01d4d76573ec666acbda10ce6929c7
Jiaxin(Peter) Liu <peterliu8368@gmail.com>
2021-08-24 15:56:44 -0700

    update

--- skynet_app/static/script.js
+++ skynet_app/static/script.js
@@ -8,13 +8,9 @@ myVideo.muted = true;
 // let peer = new Peer(undefined, {
 //     secure: true
 // });
-var peer = new Peer({ key: 'oz9b3ni30qtcsor', debug: 3, config: {'iceServers': [
-    { url: 'stun:stun.l.google.com:19302' },
-    { url: 'turn:numb.viagenie.ca:3478', credential: 'muazkh', username:'web...@live.com' },
-    { url: 'turn:numb.viagenie.ca', credential: 'muazkh', username:'web...@live.com' },
-    { url: 'turn:192.158.29.39:3478?transport=udp', credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=', username:'28224511:1379330808' },
-    { url: 'turn:192.158.29.39:3478?transport=tcp', credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=', username:'28224511:1379330808' }
-]}});
+var peer = new Peer({config: {'iceServers': [{url: 'turn:numb.viagenie.ca',
+credential: 'muazkh',
+username: 'webrtc@live.com'}]}});
 // username = prompt('Human, what is your name?')
 
 //when a user is connected to the peer server
