0f9f9cb758d821f542b9ae31daeb4928603c9c92
Jiaxin(Peter) Liu <peterliu8368@gmail.com>
2021-08-23 18:03:56 -0700

    fixed room

--- skynet_app/static/script.js
+++ skynet_app/static/script.js
@@ -1,4 +1,4 @@
-const ROOM_ID = "{{roomId}}";
+const ROOM_ID = "d9bf37c6-ab34-4153-8437-bf3bed93e275";
 const socket = io("/", { transports: ["websocket"] });
 const videoGrid = document.getElementById("video-grid");
 const myVideo = document.createElement("video");
